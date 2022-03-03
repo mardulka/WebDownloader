0701813618fa63937715bedd726dc3389ed51079
Ing. Marek Čermák <cermam22@fit.cvut.cz>
2022-03-03 18:20:51 +0100

    PR03 - Working, PROGTEST 0.1 (FULL)

--- practice/03/test.cpp
+++ practice/03/test.cpp
@@ -25,17 +25,16 @@ class record{
     string number;
 
 public:
-    string line;
     record() = default;
 
-    record(string iname, string isurname, string iline) : name{std::move(iname)}, surname{std::move(isurname)},
-                                                          line{std::move(iline)}{}
+    record(string iname, string isurname, string inumber) : name{std::move(iname)}, surname{std::move(isurname)},
+                                                            number{std::move(inumber)}{}
 
     bool check(){
-        if (number[0] == '0')
+        if (number[0] == '0' || number.length() != 9)
             return false;
         for (char const & c: number){
-            if (isalpha(c)) return false;
+            if (!isdigit(c)) return false;
         }
         return true;
     }
@@ -50,8 +49,55 @@ public:
 };
 
 ostream & operator <<(ostream & stream, const record & rec){
-    stream << rec.name << " " << rec.surname << " " << rec.number << endl;
+    stream << rec.name << " " << rec.surname << " " << rec.number;
     return stream;
+}
+
+/**
+ * Function read input from file, parse data into record class and store into vector
+ * @param file File for read data
+ * @param storage Vector for storage records
+ * @return true if ok, false if error
+ */
+bool readInput(ifstream & file, vector<record> & storage){
+    while (file){
+        //read one line (empty --> break)
+        string line;
+        getline(file, line);
+        if (line.empty()) break;
+
+        //parsing and check data
+        istringstream ss(line);
+        string name, surname, number, dummy;
+        if (!(ss >> name) || !(ss >> surname) || !(ss >> number) || ss >> dummy) return false;
+        record input(move(name), move(surname), move(number));
+        if (!input.check()) return false;
+
+        //save into vector
+        storage.push_back(input);
+    }
+    return true;
+}
+
+/**
+ * Search query in given storage and print output
+ * @param storage Vector of stored records
+ * @param query Searching string
+ * @param out output stream for printing
+ */
+void searchAndPrint(vector<record> & storage, string & query, ostream & out){
+    int cnt = 0;
+    if (query.empty()){
+        out << "-> " << cnt << endl;
+        return;
+    }
+    for (record rec: storage){
+        if (rec.find(query)){
+            out << rec << endl;
+            ++cnt;
+        }
+    }
+    out << "-> " << cnt << endl;
 }
 
 /**
@@ -67,43 +113,17 @@ bool report(const string & fileName, ostream & out){
     //open file for reading
     ifstream file;
     file.open(fileName);
-    if (!file) return false;
+    if (!file.is_open()) return false;
 
-    //cyklus cteni dat
+    // read data first
+    if (!readInput(file, storage)) return false;
 
-    while (true){
-        //precteni radku do stringu (prazdny --> break)
-        string line;
-        file >> line;
-        if (line.empty() || file.eof()) break;
-
-        //parsovani do recordu a kontroly
-        istringstream ss(line);
-        string name, surname, number, dummy;
-
-        if (!(ss >> name) || !(ss >> surname) || !(ss >> number) || ss >> dummy) return false;
-        record input(move(name), move(surname), move(number));
-        if (!input.check()) return false;
-
-        //ulozeni do vectoru
-        storage.push_back(input);
-    }
-
-
-    //cyklus cteni dotazu
-    while (!file.eof()){
+    // Search and print
+    while (file.peek() != EOF){
         string query;
-        file >> query;
-
-        // cyklus kontroly vysledku
-        int cnt = 0;
-        for (record rec: storage){
-            if (rec.find(query))
-                out << rec;
-        }
-        out << cnt << endl;
+        getline(file, query);
+        searchAndPrint(storage, query, out);
     }
-
 
     return true;
 }
@@ -114,6 +134,7 @@ int main(){
     ostringstream oss;
     oss.str("");
     assert (report("tests/test0_in.txt", oss) == true);
+    //cout << oss.str() << flush;
     assert (oss.str() ==
             "John Christescu 258452362\n"
             "John Harmson 861647702\n"
@@ -126,6 +147,20 @@ int main(){
             "-> 1\n");
     oss.str("");
     assert (report("tests/test1_in.txt", oss) == false);
+    assert (report("tests/test2_in.txt", oss) == true);
+    //cout << oss.str() << flush;
+    assert (oss.str() ==
+            "John Christescu 258452362\n"
+            "John Christescu 258452362\n"
+            "John Christescu 258452362\n"
+            "-> 3\n"
+            "-> 0\n"
+            "-> 0\n"
+            "-> 0\n"
+            "-> 0\n");
+    oss.str("");
+    assert (report("tests/test3_in.txt", oss) == false);
+    assert (report("tests/test4_in.txt", oss) == false);
     return 0;
 }
 
