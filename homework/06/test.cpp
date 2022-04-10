36b49274857cee673247ccd8035d9efe59c47f57
Ing. Marek Čermák <cermam22@fit.cvut.cz>
2022-04-10 11:31:46 +0200

    PR05 - Storing problem solved.
    - Solved problem with storing by adding virtual clone method.
    - Some problem with proper printing still present.

--- homework/06/test.cpp
+++ homework/06/test.cpp
@@ -33,15 +33,16 @@ using namespace std;
 class CDataType{
 protected:
     string m_type;
-    int m_size;
+    size_t m_size;
 public:
-    explicit CDataType(string type = "", int size = 0) : m_type{std::move(type)}, m_size{size}{}
+    explicit CDataType(string type = "", size_t size = 0) : m_type{std::move(type)}, m_size{size}{}
 
     [[nodiscard]] virtual size_t getSize() const;
     [[nodiscard]] virtual string getType() const;
     virtual bool operator ==(const CDataType & rhs) const;
     virtual bool operator !=(const CDataType & rhs) const;
-    [[nodiscard]] virtual string exportBody(const string & name, int offset) const;
+    [[nodiscard]] virtual string printBody(const string & name, int offset) const;
+    [[nodiscard]] virtual CDataType * clone() const = 0;
     friend ostream & operator <<(ostream & os, const CDataType & an_int);
 };
 
@@ -63,13 +64,13 @@ bool CDataType::operator !=(const CDataType & rhs) const{
     return !(*this == rhs);
 }
 
-string CDataType::exportBody(const string & name, int offset) const{
+string CDataType::printBody(const string & name, int offset) const{
     string text;
-    return text.append(offset, ' ').append(m_type).append(" ").append(name).append("\n");
+    return text.append(offset, ' ').append(m_type).append(" ").append(name).append(";\n");
 }
 
 ostream & operator <<(ostream & os, const CDataType & an_int){
-    os << an_int.exportBody("", 0);
+    os << an_int.printBody("", 0);
     return os;
 }
 
@@ -78,16 +79,28 @@ ostream & operator <<(ostream & os, const CDataType & an_int){
  */
 class CDataTypeInt : public CDataType{
 public:
-    explicit CDataTypeInt() : CDataType("int"s, 4){}
+    explicit CDataTypeInt() : CDataType("int", 4){}
+
+    [[nodiscard]] CDataType * clone() const override;
 };
+
+CDataType * CDataTypeInt::clone() const{
+    return new CDataTypeInt();
+}
 
 /**
  * Class for DOUBLE type
  */
 class CDataTypeDouble : public CDataType{
 public:
-    explicit CDataTypeDouble() : CDataType("double"s, 8){}
+    explicit CDataTypeDouble() : CDataType("double", 8){}
+
+    [[nodiscard]] CDataType * clone() const override;
 };
+
+CDataType * CDataTypeDouble::clone() const{
+    return new CDataTypeDouble();
+}
 
 /**
  * Class for ENUM type
@@ -102,7 +115,10 @@ public:
     CDataTypeEnum & operator =(CDataTypeEnum source);
 
     CDataTypeEnum & add(const string & name);
-    [[nodiscard]] string exportBody(const string & name, int offset) const override;
+    [[nodiscard]] string printBody(const string & name, int offset) const override;
+    bool operator ==(const CDataType & rhs) const override;
+    bool operator !=(const CDataType & rhs) const override;
+    [[nodiscard]] CDataType * clone() const override;
 };
 
 CDataTypeEnum::CDataTypeEnum(const CDataTypeEnum & source) : CDataTypeEnum(){
@@ -125,23 +141,52 @@ CDataTypeEnum & CDataTypeEnum::add(const string & name){
     return *this;
 }
 
-string CDataTypeEnum::exportBody(const string & name, int offset) const{
+string CDataTypeEnum::printBody(const string & name, int offset) const{
     string text;
     text.append(offset, ' ').append("enum\n");
     text.append(offset, ' ').append("{\n");
     for (const auto & item: options){
-        text.append(offset + 2, ' ').append(item).append("\n");
+        text.append(offset + 2, ' ').append(item);
+        if (item == options.back())
+            text.append("\n");
+        else
+            text.append(",\n");
     }
-    text.append("} ").append(name).append(";\n");
+    text.append(offset, ' ').append("} ").append(name).append(";\n");
     return text;
+}
+
+bool CDataTypeEnum::operator ==(const CDataType & rhs) const{
+    if (this->m_type != rhs.getType())
+        return false;
+    const auto & compared = dynamic_cast<const CDataTypeEnum &>(rhs);
+    if (this->options.size() != compared.options.size())
+        return false;
+    for (size_t i = 0 ; i < options.size() ; ++i){
+        if (options[i] != compared.options[i])
+            return false;
+    }
+    return true;
+}
+
+bool CDataTypeEnum::operator !=(const CDataType & rhs) const{
+    return !(*this == rhs);
+}
+
+CDataType * CDataTypeEnum::clone() const{
+    auto * clonedType = new CDataTypeEnum();
+    for (const auto & item: this->options){
+        clonedType->options.push_back(item);
+    }
+    return clonedType;
 }
 
 /**
  * Class for STRUCT type
  */
 class CDataTypeStruct : public CDataType{
-    map<string, shared_ptr<pair<string, CDataType>>> used_names;
-    vector<shared_ptr<pair<string, CDataType>>> fields;
+    map<string, shared_ptr<CDataType>> used_names;
+    vector<pair<string, shared_ptr<CDataType>>> fields;
 public:
     explicit CDataTypeStruct() : CDataType("struct"s, 0){}
 
@@ -150,17 +195,20 @@ public:
     CDataTypeStruct & operator =(CDataTypeStruct source);
 
     CDataTypeStruct & addField(const string & name, const CDataType & type);
-    CDataType & field(const string & name);
+    const CDataType & field(const string & name);
 
-    [[nodiscard]] string exportBody(const string & name, int offset) const override;
+    [[nodiscard]] string printBody(const string & name, int offset) const override;
+    [[nodiscard]] CDataType * clone() const override;
+    bool operator ==(const CDataType & rhs) const override;
+    bool operator !=(const CDataType & rhs) const override;
 };
 
 CDataTypeStruct::CDataTypeStruct(const CDataTypeStruct & source) : CDataTypeStruct(){
-    for (const auto & item: source.used_names){
-        used_names.insert(item);
-    }
     for (const auto & item: source.fields){
-        fields.push_back(item);
+        fields.emplace_back(item.first, item.second->clone());
+    }
+    for (const auto & item: fields){
+        used_names.emplace(item.first, item.second);
     }
 }
 
@@ -176,34 +224,65 @@ CDataTypeStruct & CDataTypeStruct::operator =(CDataTypeStruct source){
 }
 
 CDataTypeStruct & CDataTypeStruct::addField(const string & name, const CDataType & type){
-    if (used_names.find(name) != used_names.end())
+    if (used_names.find(name) != used_names.end()){
         throw invalid_argument("Duplicate field: " + name);
-    auto element = make_shared<pair<string, CDataType >>(make_pair(name, type));
+    }
+    auto element = shared_ptr<CDataType>(type.clone());
     used_names.insert({name, element});
-    fields.emplace_back(element);
+    fields.emplace_back(name, element);
+    m_size += type.getSize();
     return *this;
 }
 
-CDataType & CDataTypeStruct::field(const string & name){
+const CDataType & CDataTypeStruct::field(const string & name){
     auto element = used_names.find(name);
     if (element == used_names.end())
         throw invalid_argument("Unknown field: " + name);
 
-    return element->second->second;
+    return *(element->second);
 }
 
-string CDataTypeStruct::exportBody(const string & name, int offset) const{
+string CDataTypeStruct::printBody(const string & name, int offset) const{
     string text;
     text.append(offset, ' ').append("struct\n");
     text.append(offset, ' ').append("{\n");
     for (const auto & item: fields){
-        text.append(offset + 2, ' ').append(item->second.getType());
-        text.append(" ").append(item->first).append(";\n");
+        text.append(item.second->printBody(item.first, offset + 2));
     }
     text.append(offset, ' ').append("}").append(name);
     return text;
 }
 
+CDataType * CDataTypeStruct::clone() const{
+    auto * clonedType = new CDataTypeStruct();
+    for (const auto & item: fields){
+        clonedType->fields.emplace_back(item.first, shared_ptr<CDataType>(item.second->clone()));
+    }
+    for (const auto & item: clonedType->fields){
+        clonedType->used_names.emplace(item.first, item.second);
+    }
+
+    return clonedType;
+}
+
+bool CDataTypeStruct::operator ==(const CDataType & rhs) const{
+    if (this->m_type != rhs.getType())
+        return false;
+
+    const auto & compared = dynamic_cast<const CDataTypeStruct &>(rhs);
+    if (fields.size() != compared.fields.size())
+        return false;
+    for (size_t i = 0 ; i < fields.size() ; ++i){
+        if (*(fields[i].second) != *(compared.fields[i].second))
+            return false;
+    }
+
+    return true;
+}
+
+bool CDataTypeStruct::operator !=(const CDataType & rhs) const{
+    return !(*this == rhs);
+}
 
 /*
 
