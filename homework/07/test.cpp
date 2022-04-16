14face81fc5924ff539d8640c9dad82d1da96526
Ing. Marek Čermák <cermam22@fit.cvut.cz>
2022-04-16 23:30:46 +0200

    PR07 - Setup of structure

---
+++ homework/07/test.cpp
@@ -0,0 +1,36 @@
+#ifndef __PROGTEST__
+
+#include <cstring>
+#include <cstdlib>
+#include <cstdio>
+#include <cassert>
+#include <cctype>
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <set>
+#include <list>
+#include <map>
+#include <vector>
+#include <queue>
+#include <string>
+#include <algorithm>
+#include <functional>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <variant>
+#include <any>
+
+using namespace std;
+#endif /* __PROGTEST__ */
+
+template<typename T_, typename C_>
+class CIndex{
+public:
+    // constructor
+    // search ()
+    // todo
+};
+
